d319d6ad789cfa5ab6ce89a8767c03c38645b536
ndxhound <85650644+ndxhound@users.noreply.github.com>
2023-05-01 11:38:24 -0700

    Added scripts, images and package dependencies

---
+++ UIOrchestrator.Server/wwwroot/scripts/Code420Interop.js
@@ -0,0 +1,17 @@
+﻿var Code420 = Code420 || {};
+
+Code420.setDocumentTitle = function (title) {
+    document.title = title;
+};
+
+Code420.setFocus = function (element) {
+    element.focus();
+};
+
+Code420.setMaskedTextboxPlaceholder = function (labelId, labelText) {
+    document.getElementById(labelId).innerText = labelText;
+};
+
+Code420.getCssVariable = function (variable) {
+    return getComputedStyle(document.documentElement).getPropertyValue(variable);
+};
